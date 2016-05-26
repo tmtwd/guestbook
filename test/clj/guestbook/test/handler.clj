525941add5758f97ae6a785abe0930f01047a544
tmtwd <t.williams.im@gmail.com>
2016-05-26 12:54:28 -0400

    circle.yml

--- test/clj/guestbook/test/handler.clj
+++ test/clj/guestbook/test/handler.clj
@@ -1,4 +1,12 @@
 (ns guestbook.test.handler
+  (:import [org.openqa.selenium JavascriptExecutor By OutputType WebDriver WebElement WebDriverException NoAlertPresentException])
+  (:import [org.openqa.selenium.chrome ChromeDriver])
+  (:import [org.openqa.selenium.firefox FirefoxDriver FirefoxBinary FirefoxProfile])
+  ;(:import [org.openqa.selenium.htmlunit HtmlUnitDriver])
+  (:import [org.openqa.selenium.support.ui Select])
+  (:import [org.openqa.selenium.interactions Actions Action])
+  (:import [org.openqa.selenium.remote DesiredCapabilities RemoteWebDriver])
+  (:import [org.openqa.selenium.support.ui ExpectedCondition WebDriverWait])
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer :all]
             [guestbook.handler :refer :all]))
@@ -15,3 +23,13 @@
 (deftest is-broken
   (testing "that this gets triggered"
    (is (= 25 25))))
+
+(defn firefox [] (new FirefoxDriver))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (let [fd (firefox)]
+      (.get fd "http://localhost:3000")
+      (is (= "Welcome to guestbook" (.getTitle fd)))
+      (Thread/sleep 2000)
+      (.quit fd))))
